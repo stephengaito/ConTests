1c8b6616b1b24075d073eb822d51a560cde76910
Stephen Gaito <stephen@perceptisys.co.uk>
2017-07-05 05:45:51 +0100

    sketched sprintf2XXX for formating reasons

--- t-contests/tex/context/third/contests/t-contests.c
+++ t-contests/tex/context/third/contests/t-contests.c
@@ -1,3 +1,5 @@
+// An ANSI-C code file
+
 // from file: cTests.tex after line: 50
 
 void TestCaseRun(TestCase *tc) {
@@ -28,4 +30,39 @@ void ReportCAssert(TestCase *tc,
     if (tc->jumpBuf) longjump(*(tc->jumpBuf), 0);
   }
   tc->passed++;
+}
+
+#define getBuffer(bufferSize) (char*)calloc(1, bufferSize)
+
+char* sprintf2Int(const char* fmt,
+                  int intA, int intB) {
+  size_t bufferSize = strlen(fmt) + 20*2;
+  char* buffer = getBuffer(bufferSize);
+  snprintf(buffer, bufferSize, fmt, intA, intB);
+  return buffer;
+}
+
+char* sprintf2Ptr(const char* fmt,
+                  void* ptrA, void* ptrB) {
+  size_t bufferSize = strlen(fmt) + 20*2;
+  char* buffer = getBuffer(bufferSize);
+  snprintf(buffer, bufferSize, fmt, ptrA, ptrB);
+  return buffer;
+
+char* sprintf2Str(const char* fmt,
+                  const char* strA,
+                  const char* strB) {
+  size_t bufferSize = strlen(fmt)
+    + strlen(strA) + strlen(strB) + 10;
+  char* buffer = getBuffer(bufferSize);
+  snprintf(buffer, bufferSize, fmt, strA, strB);
+  return buffer;
+}
+
+char* sprintf2Dbl(const char* fmt,
+                  double dblA, double dblB) {
+  size_t bufferSize = strlen(fmt) + 20*2;
+  char* buffer = getBuffer(bufferSize);
+  snprintf(buffer, bufferSize, fmt, dblA, dblB);
+  return buffer;
 }
--- t-contests/tex/context/third/contests/t-contests.h
+++ t-contests/tex/context/third/contests/t-contests.h
@@ -1,3 +1,5 @@
+// An ANSI-C header file
+
 // from file: cTests.tex after line: 0
 
 #ifndef FALSE
@@ -34,43 +36,55 @@ extern void ReportCAssert(TestCase *tc,
                           const char* fileName,
                           int lineNum);
 
-// from file: cTests.tex after line: 100
+extern char* sprintf2Int(const char* fmt,
+                         int intA, int intB);
+extern char* sprintf2Ptr(const char* fmt,
+                         void* ptrA, void* ptrB);
+extern char* sprintf2Str(const char* fmt,
+                         const char* strA,
+                         const char* strB);
+extern char* sprintf2Dbl(const char* fmt,
+                         double dblA, double dblB);
+
+// from file: cTests.tex after line: 150
 
 #define AssertFailMsg(tc, aMessage) \
   ReportCAssert(                    \
     (tc),                           \
     FALSE,                          \
     (aMessage),                     \
-    "Assert Failed",                \
+    "Failed",                       \
     __FILE__,                       \
     __LINE__)
 
 #define AssertFail(tc) \
   AssertFailMsg(tc, "")
 
-// from file: cTests.tex after line: 100
+// from file: cTests.tex after line: 150
 
 #define AssertSucceedMsg(tc, aMessage) \
   ReportCAssert(                       \
     (tc),                              \
     TRUE,                              \
     (aMessage),                        \
-    "Assert Succeeded",                \
+    "Succeeded",                       \
     __FILE__,                          \
     __LINE__)
 
 #define AssertSucceed(tc) \
   AssertSucceedMsg(tc, "")
 
-// from file: cTests.tex after line: 100
+// from file: cTests.tex after line: 150
 
 #define AssertIntTrueMsg(tc, anInt, aMessage) \
   ReportCAssert(                              \
     (tc),                                     \
     (anInt),                                  \
     (aMessage),                               \
-    sprintf("Expected %d to be TRUE.",        \
-      (anInt)                                 \
+    sprintf2Int(                              \
+      "Expected %d to be TRUE.",              \
+      (anInt),                                \
+      NULL                                    \
     ),                                        \
     __FILE__,                                 \
     __LINE__)
@@ -78,15 +92,17 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertIntTrue(tc, anInt) \
   AssertIntTrueMsg(tc, anInt, "")
 
-// from file: cTests.tex after line: 150
+// from file: cTests.tex after line: 200
 
 #define AssertIntFalseMsg(tc, anInt, aMessage) \
   ReportCAssert(                               \
     (tc),                                      \
     !(anInt),                                  \
     (aMessage),                                \
-    sprintf("Expected %d to be FALSE.",        \
-      (anInt)                                  \
+    sprintf2Int(                               \
+      "Expected %d to be FALSE.",              \
+      (anInt),                                 \
+      NULL                                     \
     ),                                         \
     __FILE__,                                  \
     __LINE__)
@@ -94,14 +110,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertIntFalse(tc, anInt) \
   AssertIntFalseMsg(tc, anInt, "")
 
-// from file: cTests.tex after line: 150
+// from file: cTests.tex after line: 200
 
 #define AssertIntEqualsMsg(tc, intA, intB, aMessage) \
   ReportCAssert(                                     \
     (tc),                                            \
     (intA) == (intB),                                \
     (aMessage),                                      \
-    sprintf("Expected %d to be equal to %d.",        \
+    sprintf2Int(                                     \
+      "Expected %d to be equal to %d.",              \
       (intA),                                        \
       (intB)                                         \
     ),                                               \
@@ -111,14 +128,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertIntEquals(tc, intA, intB) \
   AssertIntEqualsMsg(tc, intA, intB, "")
 
-// from file: cTests.tex after line: 200
+// from file: cTests.tex after line: 250
 
 #define AssertIntNotEqualsMsg(tc, intA, intB, aMessage) \
   ReportCAssert(                                        \
     (tc),                                               \
     (intA) != (intB),                                   \
     (aMessage),                                         \
-    sprintf("Expected %d not to be equal to %d.",       \
+    sprintf2Int(                                        \
+      "Expected %d not to be equal to %d.",             \
       (intA),                                           \
       (intB)                                            \
     ),                                                  \
@@ -128,15 +146,17 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertIntNotEquals(tc, intA, intB) \
   AssertIntNotEqualsMsg(tc, intA, intB, "")
 
-// from file: cTests.tex after line: 200
+// from file: cTests.tex after line: 250
 
 #define AssertPtrNullMsg(tc, aPtr, aMessage) \
   ReportCAssert(                             \
     (tc),                                    \
     (aPtr) == (NULL),                        \
     (aMessage),                              \
-    sprintf("Expected %p to be NULL.",       \
+    sprintf2Ptr(                             \
+      "Expected %p to be NULL.",             \
       (aPtr),                                \
+      NULL                                   \
     ),                                       \
     __FILE__,                                \
     __LINE__)
@@ -144,15 +164,17 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertPtrNull(tc, aPtr) \
   AssertPtrNullMsg(tc, aPtr, "")
 
-// from file: cTests.tex after line: 200
+// from file: cTests.tex after line: 250
 
 #define AssertPtrNotNullMsg(tc, aPtr, aMessage) \
   ReportCAssert(                                \
     (tc),                                       \
     (aPtr) != (NULL),                           \
     (aMessage),                                 \
-    sprintf("Expected %p not to be NULL.",      \
+    sprintf2Pt(                                 \
+      "Expected %p not to be NULL.",            \
       (aPtr),                                   \
+      NULL                                      \
     ),                                          \
     __FILE__,                                   \
     __LINE__)
@@ -160,14 +182,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertPtrNotNull(tc, aPtr) \
   AssertPtrNotNullMsg(tc, aPtr, "")
 
-// from file: cTests.tex after line: 250
+// from file: cTests.tex after line: 300
 
 #define AssertPtrEqualsMsg(tc, ptrA, ptrB, aMessage) \
   ReportCAssert(                                     \
     (tc),                                            \
     (ptrA) == (ptrB),                                \
     (aMessage),                                      \
-    sprintf("Expected %p to be equal to %p.",        \
+    sprintf2Ptr(                                     \
+      "Expected %p to be equal to %p.",              \
       (ptrA),                                        \
       (ptrB)                                         \
     ),                                               \
@@ -177,14 +200,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertPtrEquals(tc, ptrA, ptrB) \
   AssertPtrEqualsMsg(tc, ptrA, ptrB, "")
 
-// from file: cTests.tex after line: 250
+// from file: cTests.tex after line: 300
 
 #define AssertPtrNotEqualsMsg(tc, ptrA, ptrB, aMessage) \
   ReportCAssert(                                        \
     (tc),                                               \
     (ptrA) != (ptrB),                                   \
     (aMessage),                                         \
-    sprintf("Expected %p not to be equal to %p.",       \
+    sprintf2Ptr(                                        \
+      "Expected %p not to be equal to %p.",             \
       (ptrA),                                           \
       (ptrB)                                            \
     ),                                                  \
@@ -194,15 +218,17 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertPtrNotEquals(tc, ptrA, ptrB) \
   AssertPtrNotEqualsMsg(tc, ptrA, ptrB, "")
 
-// from file: cTests.tex after line: 300
+// from file: cTests.tex after line: 350
 
 #define AssertStrEmptyMsg(tc, aStr, aMessage) \
   ReportCAssert(                              \
     (tc),                                     \
     *(aStr) == 0,                             \
     (aMessage),                               \
-    sprintf("Expected [%s] to be empty.",     \
+    sprintf2Str(                              \
+      "Expected [%s] to be empty.",           \
       (aStr),                                 \
+      NULL                                    \
     ),                                        \
     __FILE__,                                 \
     __LINE__)
@@ -210,15 +236,17 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertStrEmpty(tc, aStr) \
   AssertStrEmptyMsg(tc, aStr, "")
 
-// from file: cTests.tex after line: 300
+// from file: cTests.tex after line: 350
 
 #define AssertStrNotEmptyMsg(tc, aStr, aMessage) \
   ReportCAssert(                                 \
     (tc),                                        \
     *(aStr) != 0,                                \
     (aMessage),                                  \
-    sprintf("Expected [%s] not to be empty.",    \
+    sprintf2Str(                                 \
+      "Expected [%s] not to be empty.",          \
       (aStr),                                    \
+      NULL                                       \
     ),                                           \
     __FILE__,                                    \
     __LINE__)
@@ -226,14 +254,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertStrNotEmpty(tc, aStr) \
   AssertStrNotEmptyMsg(tc, aStr, "")
 
-// from file: cTests.tex after line: 300
+// from file: cTests.tex after line: 400
 
 #define AssertStrEqualsMsg(tc, strA, strB, aMessage) \
   ReportCAssert(                                     \
     (tc),                                            \
     strcmp((strA), (strB)) == 0,                     \
     (aMessage),                                      \
-    sprintf("Expected [%s] to be equal to [%s].",    \
+    sprintf2Str(                                     \
+      "Expected [%s] to be equal to [%s].",          \
       (strA),                                        \
       (strB)                                         \
     ),                                               \
@@ -243,14 +272,15 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertStrEquals(tc, strA, strB) \
   AssertStrEqualsMsg(tc, strA, strB, "")
 
-// from file: cTests.tex after line: 350
+// from file: cTests.tex after line: 400
 
 #define AssertStrNotEqualsMsg(tc, strA, strB, aMessage) \
   ReportCAssert(                                        \
     (tc),                                               \
     strcmp((strA), (strB)) != 0,                        \
     (aMessage),                                         \
-    sprintf("Expected [%s] not to be equal to [%s].",   \
+    sprintf2Str(                                        \
+      "Expected [%s] not to be equal to [%s].",         \
       (strA),                                           \
       (strB)                                            \
     ),                                                  \
@@ -260,37 +290,37 @@ extern void ReportCAssert(TestCase *tc,
 #define AssertStrNotEquals(tc, strA, strB) \
   AssertStrNotEqualsMsg(tc, strA, strB, "")
 
-// from file: cTests.tex after line: 350
+// from file: cTests.tex after line: 450
 
-#define AssertDblEqualsMsg(tc, dblA, dblB, tol, aMessage)     \
-  ReportCAssert(                                              \
-    (tc),                                                     \
-    fabs((dblA) - (dblB)) < (tol),                            \
-    (aMessage),                                               \
-    sprintf(                                                  \
-      "Expected %e to be equal to %e with tollerance of %e.", \
-      (dblA),                                                 \
-      (dblB)                                                  \
-    ),                                                        \
-    __FILE__,                                                 \
+#define AssertDblEqualsMsg(tc, dblA, dblB, tol, aMessage)    \
+  ReportCAssert(                                             \
+    (tc),                                                    \
+    fabs((dblA) - (dblB)) < (tol),                           \
+    (aMessage),                                              \
+    sprintf2Dbl(                                             \
+      "Expected %e to be equal to %e with tolerance of %e.", \
+      (dblA),                                                \
+      (dblB)                                                 \
+    ),                                                       \
+    __FILE__,                                                \
     __LINE__)
 
 #define AssertDblEquals(tc, dblA, dblB) \
   AssertDblEqualsMsg(tc, dblA, dblB, "")
 
-// from file: cTests.tex after line: 400
+// from file: cTests.tex after line: 450
 
-#define AssertDblNotEqualsMsg(tc, dblA, dblB, tol, aMessage)      \
-  ReportCAssert(                                                  \
-    (tc),                                                         \
-    (tol) <= fabs((dblA) - (dblB)),                               \
-    (aMessage),                                                   \
-    sprintf(                                                      \
-      "Expected %e not to be equal to %e with tollerance of %e.", \
-      (dblA),                                                     \
-      (dblB)                                                      \
-    ),                                                            \
-    __FILE__,                                                     \
+#define AssertDblNotEqualsMsg(tc, dblA, dblB, tol, aMessage)     \
+  ReportCAssert(                                                 \
+    (tc),                                                        \
+    (tol) <= fabs((dblA) - (dblB)),                              \
+    (aMessage),                                                  \
+    sprintf2Dbl(                                                 \
+      "Expected %e not to be equal to %e with tolerance of %e.", \
+      (dblA),                                                    \
+      (dblB)                                                     \
+    ),                                                           \
+    __FILE__,                                                    \
     __LINE__)
 
 #define AssertDblNotEquals(tc, dblA, dblB) \
